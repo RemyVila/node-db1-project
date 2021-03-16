69a9805c0b418de34dd004bff6d77f14289e18c9
Remy Vila <josephvila123@gmail.com>
2021-03-16 01:44:51 -0400

    ...

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,23 +1,60 @@
 const router = require('express').Router()
+const Account = require('./accounts-model');
+const {checkAccountId, checkAccountNameUnique, checkAccountPayload} = require('./accounts-middleware')
 
 router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const data = await Account.get()
+    res.json(data)
+  }
+  catch(err) {
+    next(err)
+  }
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const data = await Account.getById(req.params.id)
+    res.json(data)
+  }
+  catch(err) {
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const data = await Account.create(req.body)
+    res.json(data)
+  }
+  catch(err) {
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const data = await Account.updateById(req.params.id)
+    res.json(data)
+  }
+  catch(err) {
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const data = await Account.deleteById(req.params.id)
+    res.json(data)
+  }
+  catch(err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const accountsRoute = require('./accounts/accounts-router');
+
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/accounts', accountsRoute);
 
 module.exports = server;
